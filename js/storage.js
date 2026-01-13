63ee3b438e5570313074919bb9f09ed72b6f8761
vansh130973 <vanshapanchal@gmail.com>
2026-01-13 15:08:40 +0530

    update structure and Add a Readme file

--- js/storage.js
+++
@@ -1,5 +0,0 @@
-export const getData = (key, fallback) =>
-  JSON.parse(localStorage.getItem(key)) || fallback;
-
-export const setData = (key, value) =>
-  localStorage.setItem(key, JSON.stringify(value));
